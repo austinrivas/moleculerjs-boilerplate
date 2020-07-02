03c0dfc8d92ab381fe752d860447974ccd519c82
Austin Rivas <austinrivas@gmail.com>
2020-07-02 12:29:26 +0700

    added key store stub unit tests

--- test/Unit/Meta/JWTKeyStore.spec.ts
+++ test/Unit/Meta/JWTKeyStore.spec.ts
@@ -23,10 +23,18 @@ describe('JWTKeyStore', () => {
     expect(key.kty).toEqual('RSA');
   });
 
+  it('should parse an RSA key from a string', async () => {
+    expect(true).toBeFalsy();
+  });
+
   it('should verify an RSA key', async () => {
     const rsaKey = JWTKeyStore.readRSAKey(JWT_KEY_FILE_PATH);
     expect(JWTKeyStore.isRSAKey(rsaKey)).toEqual(true);
     expect(JWTKeyStore.isRSAKey(octKey)).toEqual(false);
+  });
+
+  it('should verify an Encryption Algorithm', async () => {
+    expect(true).toBeFalsy();
   });
 
   it('should create an RSA key', async () => {
@@ -36,5 +44,37 @@ describe('JWTKeyStore', () => {
     expect(JWTKeyStore.isRSAKey(RS256)).toEqual(true);
     expect(JWTKeyStore.isRSAKey(RS384)).toEqual(true);
     expect(JWTKeyStore.isRSAKey(RS512)).toEqual(true);
+  });
+
+  it('should save an RSA key to a pem file', async () => {
+    expect(true).toBeFalsy();
+  });
+
+  it('should get an RSA key from an variable in live environments', async () => {
+    expect(true).toBeFalsy();
+  });
+
+  it('should not persist an RSA key in live environments', async () => {
+    expect(true).toBeFalsy();
+  });
+
+  it('should get an RSA key from a generated file in dev environments', async () => {
+    expect(true).toBeFalsy();
+  });
+
+  it('should persist an RSA key in dev environments', async () => {
+    expect(true).toBeFalsy();
+  });
+
+  it('should get an RSA key from a local file in test environments', async () => {
+    expect(true).toBeFalsy();
+  });
+
+  it('should an RSA key to a key store', async () => {
+    expect(true).toBeFalsy();
+  });
+
+  it('should get an RSA key from a key store', async () => {
+    expect(true).toBeFalsy();
   });
 });
